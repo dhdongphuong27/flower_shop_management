caf7314d1cec8faaf416e566ec380743855c234d
achillescosmo <49824563+achillescosmo@users.noreply.github.com>
2022-05-05 21:37:13 +0700

    Uploading old project

---
+++ QuanLyShopHoaTuoi/QuanLyShopHoa/DAO/NhapHangDAQ.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyShopHoa.DAO
+{
+    public class NhapHangDAQ
+    {
+        private static NhapHangDAQ instance;
+        public static NhapHangDAQ Instance
+        {
+            get { if (instance == null) instance = new NhapHangDAQ(); return NhapHangDAQ.instance; }
+            private set { NhapHangDAQ.instance = value; }
+        }
+        public bool InsertNH(int id, DateTime date)
+        {
+            string query = string.Format("INSERT NhapHang (MaNH,NgayNH) VALUES ({0},N'{1}')", id, date);
+            int result = DataProvider.Instance.ExcuteNonQuery(query);
+
+            return result > 0;
+        }
+        public bool DeleteNH(int id)
+        {
+            string query = "DELETE FROM NhapHang WHERE MaNH = " + id;
+            int result = DataProvider.Instance.ExcuteNonQuery(query);
+
+            return result > 0;
+        }
+    }
+}
